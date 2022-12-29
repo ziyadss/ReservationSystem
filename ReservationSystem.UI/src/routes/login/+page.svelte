6e34666a3228073b078933d88a09d2b1ed4aebbe
Muhab Hossam <icecream10800@gmail.com>
2022-12-29 23:37:40 +0200

    Login title

--- ReservationSystem.UI/src/routes/login/+page.svelte
+++ ReservationSystem.UI/src/routes/login/+page.svelte
@@ -2,4 +2,9 @@
     import LoginComponent from "../../components/LoginComponent.svelte";
 </script>
 
+<svelte:head>
+    <title>Login & Start your journey!</title>
+    <meta name="description" content="Qatar 2022&trade;">
+</svelte:head>
+
 <LoginComponent />
