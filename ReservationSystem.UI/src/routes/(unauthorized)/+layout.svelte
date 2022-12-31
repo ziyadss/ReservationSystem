e265bffd563fd9491b877385445dede0655b7495
Na-daT <nadatarek2710@gmail.com>
2022-12-31 22:28:23 +0200

    directory changes

---
+++ ReservationSystem.UI/src/routes/(unauthorized)/+layout.svelte
@@ -0,0 +1,12 @@
+<script
+	type="text/javascript"
+	src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.0.1/mdb.min.js"
+>
+	import '../styles.css';
+</script>
+
+<body class="bg">
+	<main class="main">
+		<slot />
+	</main>
+</body>
