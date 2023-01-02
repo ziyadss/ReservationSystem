5ae9f66703169321c58c284cb0b740b2823fbb5b
Muhab Hossam <icecream10800@gmail.com>
2023-01-02 03:48:51 +0200

    Update +page.svelte

--- ReservationSystem.UI/src/routes/(app)/schedule/+page.svelte
+++ ReservationSystem.UI/src/routes/(app)/schedule/+page.svelte
@@ -7,22 +7,6 @@
 	export let data: PageData;
 
 	let matches = data.matches;
-	// Scroll button scripting
-	/*
-    let scroll_btn = document.querySelector('.scroll-top');
-    window.addEventListener('scroll', () => {
-        if (window.scrollY > 200) {
-            scroll_btn!.classList.add('active');
-        } else {
-            scroll_btn!.classList.remove('active');
-        }
-    });
-    scroll_btn!.addEventListener('click', () => {
-        document.documentElement.scrollIntoView({
-            behavior: 'smooth',
-        });
-    });
-    */
 	function navigateBook(id: number) {
 		window.location.replace('/reservation/' + id);
 	}
